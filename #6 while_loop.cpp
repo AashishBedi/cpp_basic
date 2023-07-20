8cb5df1a2c677699ea356d24be2c39dadb4661c5
Aashish Bedi <120207125+AashishBedi@users.noreply.github.com>
2023-07-20 15:15:04 +0530

    Add files via upload

---
+++ #6 while_loop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int index;
+	
+	
+	while(index<10){
+		cout<<"We are at index: "<<index<<endl;
+		index = index+1;
+	}
+	return 0;
+} 
---
+++ #7 do-while_loop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int index=0;
+	
+	
+	do{
+		cout<<"We are at index: "<<index<<endl;
+		index = index+1;
+	}while(index<3453);
+	return 0;
+} 
